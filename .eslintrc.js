a60ae35f41e0a6cd69ea0ce4076bd65e941f0bd3
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-02-26 20:39:54 +0700

    feat: Add more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
     'node': true,
   },
   'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'google',
   ],
   'globals': {
@@ -17,12 +20,37 @@ module.exports = {
     'sourceType': 'module',
   },
   'plugins': [
-    '@typescript-eslint',
+    'jsdoc',
   ],
   'rules': {
-    'linebreak-style': [
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1,
+      },
+    ],
+    'linebreak-style': [ // change this accordingly
       'warn',
-      'windows'
-    ]
+      'windows',
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always',
+    ],
+    'arrow-parens': [
+      'error',
+      'as-needed',
+      {
+        'requireForBlockBody': true,
+      },
+    ],
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+    'new-cap': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-invalid-this': 'off',
+    'require-atomic-updates': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   },
 };
